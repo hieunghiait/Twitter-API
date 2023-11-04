5d4551f47c32ccc8bfe07e31dbfa00a887f5c2f1
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-11-05 02:24:07 +0700

    Upload comment

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -3,6 +3,7 @@ import { createHash } from 'crypto'
 export function sha256(content: string) {
   return createHash('sha256').update(content).digest('hex')
 }
+//Function to hash password before saving to database
 export function hashPassword(password: string) {
   return sha256(password + process.env.PASSWORD_SECRECT)
 }
